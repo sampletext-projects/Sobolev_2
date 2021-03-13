aa03ccfb3ca2d40d9a428c0290ad14ee9c0c0737
Bird Egop <sampletext32@bk.ru>
2021-03-14 00:12:51 +0300

    Add project

---
+++ Sobolev_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Sobolev_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = double.Parse(Console.ReadLine());
+
+            double z;
+            if (b > 8)
+            {
+                z = b;
+            }
+            else if (b == 8)
+            {
+                z = b * b * b;
+            }
+            else
+            {
+                z = Math.Pow(b, 4);
+            }
+
+
+            double a;
+            if (b > 6)
+            {
+                a = b * b + Math.E;
+            }
+            else
+            {
+                a = b * b - Math.Cos(Math.E);
+            }
+
+
+            double Y;
+            if (a * z > 15)
+            {
+                Y = 5;
+            }
+            else if (a * z == 15)
+            {
+                Y = b * Math.Cos(a);
+            }
+            else
+            {
+                Y = Math.Tan(a) + b * b;
+
+            }
+
+
+            Console.WriteLine(Y);
+        }
+    }
+}
